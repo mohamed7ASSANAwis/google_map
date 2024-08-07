deb64d331edc0f6658e9bc635c944676f9246dbe
MOHAMED7ASSA6 <mh5093351@gmail.com>
2024-08-07 17:34:18 +0300

    first initMarkers

--- lib/customs_camera.dart
+++ lib/customs_camera.dart
@@ -1,6 +1,8 @@
-import 'package:flutter/cupertino.dart';
+import 'package:ctreate_google_map/models/place_model.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'dart:ui' as ui;
 
 class CustomsCameraPosition extends StatefulWidget {
   const CustomsCameraPosition({super.key});
@@ -20,7 +22,7 @@ class _CustomsCameraPositionState extends State<CustomsCameraPosition> {
           29.054357700053103,
           31.156642102513437,
         ));
-    Markers();
+    initMarkers();
     super.initState();
   }
 
@@ -33,12 +35,23 @@ class _CustomsCameraPositionState extends State<CustomsCameraPosition> {
     super.dispose();
   }
 
+  // Future<Uint8List>getImageFromRawData(String image,double widget)async{
+  //   var imageData=await rootBundle.load(image);
+  //   var imageCode=await ui.instantiateImageCodec(
+  //     imageData.buffer.asUint8List(),
+  //     targetWidth: widget.round(),
+  //
+  //   );
+  //   var imageFrame=await imageCode.getNextFrame();
+  //   var imageByteData=await imageFrame.image.toByteData(format: ui.ImageByteFormat.png);
+  //   return imageByteData!.buffer.asUint8List();
+  // }
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: [
         GoogleMap(
-          markers: markers,
+            markers: markers,
             //mapType: MapType.hybrid,
             onMapCreated: (controller) {
               googleMapController = controller;
@@ -46,17 +59,17 @@ class _CustomsCameraPositionState extends State<CustomsCameraPosition> {
             },
             //  cameraTargetBounds: CameraTargetBounds(LatLngBounds(northeast:LatLng(31.335475153105723,31.277650507997336, ) ,southwest:LatLng(31.13000568395708, 30.97950683212612) )),
             initialCameraPosition: initialCameraPosition),
-        Positioned(
-          right: 16,
-          left: 16,
-          bottom: 16,
-          child: ElevatedButton(
-              onPressed: () {
-                googleMapController.animateCamera(CameraUpdate.newLatLng(
-                    LatLng(31.072308396346227, 29.933525474820893)));
-              },
-              child: Text("Change Location")),
-        )
+        // Positioned(
+        //   right: 16,
+        //   left: 16,
+        //   bottom: 16,
+        //   child: ElevatedButton(
+        //       onPressed: () {
+        //         googleMapController.animateCamera(CameraUpdate.newLatLng(
+        //             LatLng(31.072308396346227, 29.933525474820893)));
+        //       },
+        //       child: Text("Change Location")),
+        // )
       ],
     );
   }
@@ -67,10 +80,22 @@ class _CustomsCameraPositionState extends State<CustomsCameraPosition> {
     googleMapController.setMapStyle(nightStyle);
   }
 
-  void Markers() {
-    var myMarkers= Marker(markerId: MarkerId("1"),position: LatLng(31.072308396346227, 29.933525474820893));
-    markers.add(myMarkers);
-
+  void initMarkers() async {
+    // var customMarkerIcon=BitmapDescriptor.fromBytes(
+    //
+    //   await getImageFromRawData("assets/images/location-icon.jpg", 100)
+    // );
+    var iconsBitMap = await BitmapDescriptor.fromAssetImage(
+        ImageConfiguration(), "assets/images/icon_location1.png");
+    var myMarkers = await place
+        .map((placeModel) => Marker(
+            icon: iconsBitMap,
+            infoWindow: InfoWindow(title: placeModel.name.toString()),
+            markerId: MarkerId(placeModel.id.toString()),
+            position: placeModel.latLng))
+        .toSet();
+    markers.addAll(myMarkers);
+    setState(() {});
   }
 }
 
